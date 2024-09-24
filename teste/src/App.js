8c1c4550c9df7f3fdd5c93e2312d24eeee7b1a8f
Danieldcv <d260398@dac.unicamp.br>
2024-09-24 10:51:51 -0300

    Refatorando as perguntas

--- teste/src/App.js
+++ teste/src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
-import LoginPage from "./pages/LoginPage";
-import CadastroPage from "./pages/CadastroPage";
-import Arrojado from "./pages/Arrojado";
-import Conservador from "./pages/Conservador";
-import Moderado from "./pages/Moderado";
-import Pergunta1 from "./pages/Pergunta1";
-import Pergunta2 from "./pages/Pergunta2";
-import Pergunta3 from "./pages/Pergunta3";
-import Pergunta4 from "./pages/Pergunta4";
-import Pergunta5 from "./pages/Pergunta5";
+// import LoginPage from "./pages/LoginPage";
+// import CadastroPage from "./pages/CadastroPage";
+// import Arrojado from "./pages/Arrojado";
+// import Conservador from "./pages/Conservador";
+// import Moderado from "./pages/Moderado";
+import Perguntas from "./pages/Perguntas";
 
 function App() {
   return (
@@ -22,11 +18,7 @@ function App() {
     // <Moderado/>
 
     /* perguntas do quiz */
-    // <Pergunta1/>
-    // <Pergunta2/>
-    // <Pergunta3/>
-    // <Pergunta4/>
-    <Pergunta5/>
+    <Perguntas/>
   );
 }
 
---
+++ teste/src/pages/Perguntas.js
@@ -0,0 +1,103 @@
+import React, {useState} from 'react'
+import '../styles/perguntas.css'
+
+const perguntas = [
+    {
+        'pergunta': "1. Qual é o seu principal objetivo ao investir?",
+        'alternativas': [
+            "a) Preservar meu capital, mesmo que o retorno seja menor.",
+            "b) Obter um equilíbrio entre segurança e crescimento.",
+            "c) Buscar altos retornos, mesmo que isso signifique correr riscos maiores."
+        ]
+    },
+    {
+        'pergunta': "2. Em quanto tempo você espera recuperar seus investimentos?",
+        'alternativas': [
+            "a) Curto prazo (menos de 3 anos).",
+            "b) Médio prazo (entre 3 e 5 anos).",
+            "c) Longo prazo (mais de 5 anos)."
+        ]
+    },
+    {
+        'pergunta': "3. Em que tipo de investimentos você se sente mais confortável?",
+        'alternativas': [
+            "a) Títulos de renda fixa e CDBs.",
+            "b) Fundos multimercado que combinam renda fixa e variável.",
+            "c) Ações, fundos de investimento agressivos e criptomoedas."
+        ]
+    },
+    {
+        'pergunta': "4. Como você se sentiria se seus investimentos sofressem uma queda de 20% em um curto período?",
+        'alternativas': [
+            "a) Muito preocupado. Consideraria vendê-los para evitar um prejuízo maior.",
+            "b) Preocupado. No entanto, manteria os investimentos esperando uma recuperação.",
+            "c) Tranquilo. Enxergaria isso como uma oportunidade para comprar mais ativos."
+        ]
+    },
+    {
+        'pergunta': "5. Quais opções você considera ao pensar em um investimento?",
+        'alternativas': [
+            "a) Prefiro opções já conhecidas e seguras, nas quais sei exatamente o que esperar.",
+            "b) Gosto de considerar novas oportunidades, mas prefiro avaliar bem antes de decidir.",
+            "c) Estou sempre buscando novas ideias, disposto a experimentar algo inovador e arriscado."
+        ]
+    },
+]
+
+const Perguntas = () => {
+    const [i, setI] = useState(0);
+    const handleClick = () => {
+        if (i < perguntas.length - 1) {
+            setI(i + 1);
+        }
+        else {
+            // implementar direcionamento para tela de resultados
+        }
+    };
+    const handleOptionChange = (event) => {
+        const options = document.getElementsByName('investment-question');
+        options.forEach(option => {
+          option.parentNode.classList.remove('selected'); 
+        });
+        event.target.parentNode.classList.add('selected'); 
+    };
+    
+      return (
+        <div className="container">
+          <div className="question-box">
+            <p className="question">
+              <strong> {perguntas[i].pergunta} </strong>
+            </p>
+            <div className="options">
+            <label className="option">
+            <input
+                  type="radio"
+                  name="investment-question"
+                  onChange={handleOptionChange}
+            />
+            <span> {perguntas[i].alternativas[0]} </span>
+            </label>
+            <label className="option">
+            <input
+                type="radio"
+                name="investment-question"
+                onChange={handleOptionChange}
+            />
+            <span> {perguntas[i].alternativas[1]} </span>
+            </label>
+            <label className="option">
+            <input
+                  type="radio"
+                  name="investment-question"
+                  onChange={handleOptionChange}
+            />
+            <span> {perguntas[i].alternativas[2]} </span>
+            </label>
+        </div>
+        <button className="continue-button" onClick={handleClick}> {i == perguntas.length - 1 ? "Finalizar" : "Continuar"} </button>
+        </div>
+    </div>
+    )
+}
+
+export default Perguntas
