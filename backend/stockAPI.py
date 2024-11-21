88e1047c843edbe59f8237034c66560ebf8e2b8f
JoaoMKlGui <j260542@dac.unicamp.br>
2024-11-21 15:00:15 -0300

    criacao do quantbot e criacao da funcao na api

---
+++ backend/stockAPI.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import yfinance as yf
+from typing import List
+import numpy as np
+
+def getTickers(tickers: List, periodDefined = '10y') -> List: 
+  stocks = []
+  for ticker in tickers:
+    stock_info = yf.Ticker(ticker).history(period=periodDefined)
+    stock_info['Stock Name'] = ticker
+    stock_info.dropna(inplace=True)
+    stocks.append(stock_info)
+
+  return stocks
+
+def getRelativeReturn(stocks: List):
+  for stock in stocks:
+    stock['Relative Returns'] = (stock['Close'] / stock['Open']) - 1
+  
+  return stocks
+
+def movingAvarageCalculator(stocks: List, windowSizeFast = 42, windowSizeSlow = 200) -> List:
+  for stock in stocks:
+    stock['MAF'] = stock['Close'].rolling(windowSizeFast).mean()
+    stock['MAS'] = stock['Close'].rolling(windowSizeSlow).mean()
+    stock.dropna(inplace=True)
+  return stocks
+
+def getSignals(stocks: List):
+  for stock in stocks:
+    stock['Signal'] = np.where(stock['MAF'] > stock['MAS'], 1, -1)
+    stock.dropna(inplace=True)
+  
+  return stocks
+
+def getReturns(stocks: List):
+  for stock in stocks:
+    stock['Strategy Return'] = stock['Relative Returns'] * stock['Signal']
+    stock.dropna(inplace=True)
+
+  return stocks
+
+def getTotalReturn(stocks: List):
+  for stock in stocks:
+    stock['Total Return'] = 100 * (stock['Strategy Return'].cumsum() - 1)
+
+  return stocks
+
+def applyStrategy(tickersList: List):
+  stocks = getTickers(tickersList)
+  stocks = getRelativeReturn(stocks)
+  stocks = movingAvarageCalculator(stocks)
+  stocks = getSignals(stocks)
+  results = getReturns(stocks)
+  results = getTotalReturn(results)
+  return results
