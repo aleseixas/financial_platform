e10c8954719a9fc7a73c6bbef3b4ee42d4e51f18
gviterbo <g217600@dac.unicamp.br>
2024-11-26 17:14:48 -0300

    fixing css and reviving our home page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../styles/homepage.css';
+import Navbar from '../components/Navbar';
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+  
+  
+  const handleButtonClick = (event) => {
+    
+  };
+  
+  
+  
+  
+  
+  
+  
   return (
-    <div className='Home'>
-      Home
+    <>
+    <Navbar />
+    <div className = "container-brabo-da-home">
+    <div className="feature">
+        <button className="feature-b" onClick={handleButtonClick}>QuantBot</button>
+        <button className="feature-b" onClick={handleButtonClick}>Leituras</button>
+        <button className="feature-b" onClick={handleButtonClick}>Notícias</button>
+        <button className="feature-b" onClick={handleButtonClick}>Cursos</button>
+        <button className="feature-b" onClick={handleButtonClick}>Qui</button>
+        
+      </div>
     </div>
-  )
+    </>
+
+    
+  );
 }
 
 export default HomePage
--- frontend/src/pages/TagsLeitura.js
+++ frontend/src/pages/TagsLeitura.js
@@ -140,7 +140,7 @@ const TagsLeitura = () => {
     <>
       <Navbar />
 
-      <div className="container">
+      <div className="container-brabo">
       <div className="tags">
         <button className="tag-b" onClick={handleButtonClick}>Finanças</button>
         <button className="tag-b" onClick={handleButtonClick}>Mercado Financeiro</button>
@@ -152,6 +152,8 @@ const TagsLeitura = () => {
         <button className="tag-b" onClick={handleButtonClick}>Câmbio</button>
         <button className="tag-b" onClick={handleButtonClick}>Investimentos</button>
       </div>
+
+
       <div className='Dificuldade'>
 
         <button className='nivel-i' onClick={handleButtonClick}>Iniciante</button>
@@ -159,6 +161,8 @@ const TagsLeitura = () => {
         <button className='nivel-a' onClick={handleButtonClick}>Avançado</button>
 
       </div>
+
+      
       <div className="content">
         {activeText ? (
           <div className="content-text">{activeText}</div>
