4f58ade8a6a564fc9a30934e421af47b89ce5147
gviterbo <g217600@dac.unicamp.br>
2024-11-29 11:19:36 -0300

    adding tests

--- frontend/src/__tests__/CadastroPage.test.js
+++ frontend/src/__tests__/CadastroPage.test.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import Cadastro from '../pages/CadastroPage'; // Caminho do seu componente Cadastro
+import '@testing-library/jest-dom/extend-expect';
+import Cadastro from './Cadastro'; // Adjust the import path as necessary
+import { BrowserRouter } from 'react-router-dom';
+
+const renderCadastro = () => {
+  render(
+    <BrowserRouter>
+      <Cadastro />
+    </BrowserRouter>
+  );
+};
 
 describe('Cadastro Component', () => {
   
   describe('Validação de Data de Nascimento', () => {
-
+    
     test('deve permitir data válida dentro do intervalo (valores corretos)', () => {
-      render(<Cadastro />);
+      renderCadastro();
       
       userEvent.type(screen.getByLabelText(/dia/i), '15');
       userEvent.type(screen.getByLabelText(/mês/i), '06');
@@ -20,7 +29,7 @@ describe('Cadastro Component', () => {
     });
 
     test('não deve permitir ano abaixo de 1900 (valor incorreto - limite inferior)', () => {
-      render(<Cadastro />);
+      renderCadastro();
       
       userEvent.type(screen.getByLabelText(/dia/i), '15');
       userEvent.type(screen.getByLabelText(/mês/i), '06');
@@ -32,7 +41,7 @@ describe('Cadastro Component', () => {
     });
 
     test('não deve permitir ano superior ao atual (valor incorreto - limite superior)', () => {
-      render(<Cadastro />);
+      renderCadastro();
       
       userEvent.type(screen.getByLabelText(/dia/i), '15');
       userEvent.type(screen.getByLabelText(/mês/i), '06');
@@ -44,7 +53,7 @@ describe('Cadastro Component', () => {
     });
 
     test('não deve permitir mês maior que 12 (valor incorreto - mês inválido)', () => {
-      render(<Cadastro />);
+      renderCadastro();
       
       userEvent.type(screen.getByLabelText(/dia/i), '15');
       userEvent.type(screen.getByLabelText(/mês/i), '13');
@@ -56,7 +65,7 @@ describe('Cadastro Component', () => {
     });
 
     test('não deve permitir dia maior que 31 (valor incorreto - dia inválido)', () => {
-      render(<Cadastro />);
+      renderCadastro();
       
       userEvent.type(screen.getByLabelText(/dia/i), '32');
       userEvent.type(screen.getByLabelText(/mês/i), '06');
@@ -66,19 +75,5 @@ describe('Cadastro Component', () => {
 
       expect(screen.getByText(/data de nascimento inválida/i)).toBeInTheDocument();
     });
-
-    test('não deve permitir data inválida (data não existe no calendário, ex: 31 de fevereiro)', () => {
-      render(<Cadastro />);
-      
-      userEvent.type(screen.getByLabelText(/dia/i), '31');
-      userEvent.type(screen.getByLabelText(/mês/i), '02');
-      userEvent.type(screen.getByLabelText(/ano/i), '1990');
-
-      fireEvent.click(screen.getByText(/cadastre-se/i));
-
-      expect(screen.getByText(/data de nascimento inválida/i)).toBeInTheDocument();
-    });
-
   });
-
 });
