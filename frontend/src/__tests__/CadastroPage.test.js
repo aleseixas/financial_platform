169dd2ba909df9371b42823a1b492064a6095fde
gviterbo <g217600@dac.unicamp.br>
2024-11-29 11:22:17 -0300

    adding tests

--- frontend/src/__tests__/CadastroPage.test.js
+++ frontend/src/__tests__/CadastroPage.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import Cadastro from './Cadastro'; // Adjust the import path as necessary
+import Cadastro from '../pages/CadastroPage'; // Adjust the import path as necessary
 import { BrowserRouter } from 'react-router-dom';
 
 const renderCadastro = () => {
