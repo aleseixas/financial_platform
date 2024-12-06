b33b4200ffcad3809dd44898dba376fb5b326e22
gviterbo <g217600@dac.unicamp.br>
2024-12-06 20:01:21 -0300

    adding some tests

---
+++ backend/test_main.py
@@ -0,0 +1,10 @@
+import pytest
+from fastapi.testclient import TestClient
+from main import app # Adjust the import path as needed
+
+
+
+# test_mock.py
+
+def test_will_always_passes():
+    assert True  # This will always pass
